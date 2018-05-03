97dba6d91564a59c29fd62667e85dea6c75f007a
diogo <dgo.corinthias@hotmail.com>
2018-05-03 19:47:01 -0300

    consegui passa detalhes para carrinho - em desenvolvimento

--- LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/SessaoController.java
+++ LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/SessaoController.java
@@ -1,10 +1,14 @@
 package br.com.PIIVLivrariaAstec.LivrariaAstec.Controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,9 +20,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Scope("session")
 public class SessaoController implements Serializable{
     
+    private List<Integer> numerosDigitados = new ArrayList<>();
+            
+    
     @GetMapping
     public ModelAndView mostrarTela(){
-        return new ModelAndView("carrinho");
+        System.out.println("Get Sessao");
+        numerosDigitados.add(23);
+        numerosDigitados.add(25);
+        System.out.println(numerosDigitados.isEmpty());
+        return new ModelAndView("Carrinho");
+    }
+    
+    @PostMapping
+    public ModelAndView addProdutosCarrinho(
+            @ModelAttribute("numero") Integer numero){
+        System.out.println("Post Sessao");
+        numerosDigitados.add(numero);
+        return new ModelAndView("Carrinho");
+    }
+    
+    public List<Integer> getNumerosDigitados() {
+        return numerosDigitados;
     }
     
 }
