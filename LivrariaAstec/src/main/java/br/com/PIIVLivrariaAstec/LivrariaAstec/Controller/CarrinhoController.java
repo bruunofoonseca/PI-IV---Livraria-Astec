023df9560c6996aa1f5b83ff879e4c289cb06b10
BRUNO FONSECA DE ALMEIDA <bruno.falmeida@senacsp.edu.br>
2018-05-10 22:27:18 -0300

    remover item do carrinho

--- LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/CarrinhoController.java
+++ LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/CarrinhoController.java
@@ -10,7 +10,6 @@ import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.ItemPedidoModel;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.PedidoTemp;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.ProdutoModel;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.service.ProdutoService;
-import br.com.PIIVLivrariaAstec.LivrariaAstec.service.JpaImpl.ProdutoServiceJpaImpl;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -23,20 +22,18 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  *
- * @author Bruno Fonseca
- * edit diogo.Sfelix
+ * @author Bruno Fonseca edit diogo.Sfelix
  */
 @Controller
 @RequestMapping("/Carrinho")
 @Scope("session")
 public class CarrinhoController implements Serializable {
-  
+
     @Autowired
     private ProdutoService service;
 
@@ -45,48 +42,39 @@ public class CarrinhoController implements Serializable {
     @GetMapping
     public ModelAndView mostrarCarrinho() {
         List<ItemIds> temp = new ArrayList<>();
-        for (ItemPedidoModel item : itens) {
-            ItemIds i = new ItemIds();
-            i.setIdProduto(item.getProduto().getId());
-            i.setQtd(item.getQtd());
-            temp.add(i);
+        if(!itens.isEmpty()) {
+            for (ItemPedidoModel item : itens) {
+                ItemIds i = new ItemIds();
+                i.setIdProduto(item.getProduto().getId());
+                i.setQtd(item.getQtd());
+                temp.add(i);
+            }
         }
+
         PedidoTemp ptemp = new PedidoTemp();
         ptemp.setItems(temp);
         return new ModelAndView("Carrinho").addObject("ptemp", ptemp);
     }
 
     @PostMapping
-    public ModelAndView adicionarProduto(@ModelAttribute("numero") Long id, 
+    public ModelAndView adicionarProduto(@ModelAttribute("numero") Long id,
             RedirectAttributes redirectAttributes) {
-       /* 
-      ProdutoModel p = service.obter(id);
-      
-      ItemPedidoModel item = new ItemPedidoModel();
-      item.setProduto(p);
-      item.setQtd(1);
-      item.setValorParcial(p.getValorProduto());
-      
-      this.itens.add(item);
-      */
-        
         ProdutoModel p = service.obter(id);
-      
+
         ItemPedidoModel item = new ItemPedidoModel();
-        
-        if(!itens.isEmpty()) {
-//            List<ItemPedidoModel> aux = itens;
+
+        if (!itens.isEmpty()) {
             boolean adicionou = false;
-            for (ItemPedidoModel i : itens){
-                if(i.getProduto().getId() == id){
-                    i.setQtd(i.getQtd()+1);
+            for (ItemPedidoModel i : itens) {
+                if (i.getProduto().getId() == id) {
+                    i.setQtd(i.getQtd() + 1);
                     i.setValorParcial(i.getProduto().getValorProduto().multiply(BigDecimal.valueOf(i.getQtd())));
                     adicionou = true;
                     break;
                 }
             }
-            
-            if(!adicionou) {
+
+            if (!adicionou) {
                 item.setProduto(p);
                 item.setQtd(1);
                 item.setValorParcial(p.getValorProduto());
@@ -97,29 +85,42 @@ public class CarrinhoController implements Serializable {
             item.setProduto(p);
             item.setQtd(1);
             item.setValorParcial(p.getValorProduto());
-                
+
             this.itens.add(item);
         }
 
         // POST-REDIRECT-GET
         return new ModelAndView("redirect:/Carrinho");
     }
-      
+
     @PostMapping("/validandoPedido")
     public ModelAndView teste(
             @ModelAttribute("ptemp") PedidoTemp ptemp,
-            RedirectAttributes redirectAttributes
-    ){
-        
-        for(ItemPedidoModel i : itens){
-            for(ItemIds w : ptemp.getItems()){
-                if(i.getProduto().getId() == w.getIdProduto()){
+            RedirectAttributes redirectAttributes) {
+
+        for (ItemPedidoModel i : itens) {
+            for (ItemIds w : ptemp.getItems()) {
+                if (i.getProduto().getId() == w.getIdProduto()) {
                     i.setQtd(w.getQtd());
                     i.setValorParcial(i.getProduto().getValorProduto().multiply(BigDecimal.valueOf(w.getQtd())));
                 }
             }
         }
-        
+
         return new ModelAndView("redirect:/login");
+    }
+    
+    @GetMapping("/{id}")
+    public ModelAndView Remove(@PathVariable("id") Long id,
+            RedirectAttributes redirectAttributes) {
+
+        for (ItemPedidoModel i : itens) {
+                if (i.getProduto().getId() == id) {
+                    itens.remove(i);
+                    break;
+                }
+        }
+
+        return new ModelAndView("redirect:/Carrinho");
     }
 }
