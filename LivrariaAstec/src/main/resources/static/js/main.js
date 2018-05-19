ed5e2d89e27dbb08c2ed0feb1abb1aae1b23bb9b
bruunofoonseca <bruuno.foonseca@hotmail.com>
2018-05-19 12:20:16 -0300

    Formatando campo de CPF e Telefones

--- LivrariaAstec/src/main/resources/static/js/main.js
+++ LivrariaAstec/src/main/resources/static/js/main.js
@@ -1,14 +1,19 @@
-
-$(document).ready(function() { 
-    ajustar();
-    $('#validationDate').mask('00/00/0000');
-    $('#validationCustom03').mask('(00) 0000-0000');
-    $('#validationCustom04').mask('(00) 00000-0000');
-    $('#validationCPF').mask('000.000.000-00', {reverse: true});
-    $('#validationCep').mask('00000-000');
-});
-
-function ajustar(){
-    var altura = $("#navFixed").css("height");
-    $(".ajustar").css("margin-top",altura);
-}
+//
+//$(document).ready(function() { 
+//    ajustar();
+//    $('#validationDate').mask('00/00/0000');
+//    $('#validationCustom03').mask('(00) 0000-0000');
+//    $('#validationCustom04').mask('(00) 00000-0000');
+//    $('#validationCPF').mask('000.000.000-00', {reverse: true});
+//    $('#validationCep').mask('00000-000');
+//});
+//
+//$('#validationCPF').formatter({
+//  'pattern': '{{9999}}-{{9999}}-{{9999}}-{{9999}}'
+//});
+//
+//
+//function ajustar(){
+//    var altura = $("#navFixed").css("height");
+//    $(".ajustar").css("margin-top",altura);
+//}
