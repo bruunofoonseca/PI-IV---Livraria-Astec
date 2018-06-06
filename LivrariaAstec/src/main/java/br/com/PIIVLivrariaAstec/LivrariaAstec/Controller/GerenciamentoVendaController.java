436799a7ccf43d24a1e337e6cd90b0c2d96297ab
Bruno Fonseca de Almeida <bruno.almeida@M-REND-SIST-01.local>
2018-06-06 12:32:52 -0300

    Ajustes no cadastro de produto

--- LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/GerenciamentoVendaController.java
+++ LivrariaAstec/src/main/java/br/com/PIIVLivrariaAstec/LivrariaAstec/Controller/GerenciamentoVendaController.java
@@ -5,6 +5,7 @@
  */
 package br.com.PIIVLivrariaAstec.LivrariaAstec.Controller;
 
+import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.CategoriaModel;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,13 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.PedidoModel;
 import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.ProdutoModel;
-import br.com.PIIVLivrariaAstec.LivrariaAstec.Models.UsuarioModel;
+import br.com.PIIVLivrariaAstec.LivrariaAstec.service.CategoriaService;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 /**
  *
  * @author diogo.Sfelix
@@ -27,36 +29,44 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 @RequestMapping("/gerenciamento")
 public class GerenciamentoVendaController {
-    
+
     @Autowired
     private PedidoService servicePedido;
-    
+
+    @Autowired
+    private CategoriaService serviceCategoria;
+
     @GetMapping()
-    public ModelAndView listarVenda(){
-        
-        List<PedidoModel> lista  = servicePedido.listar();
-        
+    public ModelAndView listarVenda() {
+        List<PedidoModel> lista = servicePedido.listar();
+
         return new ModelAndView("vendasRealizadas").addObject("vendas", lista);
     }
-    
+
     @GetMapping("/addProduto")
-    public ModelAndView cadastrarProduto(){
+    public ModelAndView cadastrarProduto() {
         ProdutoModel produto = new ProdutoModel();
-        
-        return new ModelAndView("cadastroProduto").addObject("produto",produto);
+        List<CategoriaModel> categorias = serviceCategoria.listar();
+
+        return new ModelAndView("cadastroProduto")
+                .addObject("produto", produto)
+                .addObject("categorias", categorias);
     }
-            
+
     @PostMapping("/addProduto/gravar")
     public ModelAndView gravarProduto(
-    @Valid @ModelAttribute("produto") ProdutoModel produto,
-        BindingResult bindingResult,
-        RedirectAttributes redirectAttributes
-    ){
+            @Valid @ModelAttribute("produto") ProdutoModel produto,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes
+    ) {
         if (bindingResult.hasErrors()) {
-            return new ModelAndView("/addProduto");
+            List<CategoriaModel> categorias = serviceCategoria.listar();
+
+            return new ModelAndView("cadastroProduto")
+                .addObject("produto", produto)
+                .addObject("categorias", categorias);
         }
-        
+
         return new ModelAndView("/");
     }
-    
 }
